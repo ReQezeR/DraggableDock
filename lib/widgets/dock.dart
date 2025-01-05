c65e1c1d031e926ff42a57c65d92b36f25a0c053
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2025-01-05 17:47:12 +0100

    add dock item widget

--- lib/widgets/dock.dart
+++ lib/widgets/dock.dart
@@ -1,5 +1,5 @@
 import 'package:animated_reorderable_list/animated_reorderable_list.dart';
-import 'package:draggable_dock/widgets/hoverable_wrapper.dart';
+import 'package:draggable_dock/widgets/dock_item.dart';
 import 'package:flutter/material.dart';
 
 class Dock<T extends Object> extends StatefulWidget {
@@ -7,6 +7,7 @@ class Dock<T extends Object> extends StatefulWidget {
     super.key,
     this.items = const [],
     required this.builder,
+    this.itemHeight = 64,
   });
 
   /// Initial [T] items to put in this [Dock].
@@ -15,39 +16,24 @@ class Dock<T extends Object> extends StatefulWidget {
   /// Builder building the provided [T] item.
   final Widget Function(T, double) builder;
 
+  /// DockItem height
+  final double itemHeight;
+
   @override
   State<Dock<T>> createState() => _DockState<T>();
 }
 
 /// State of the [Dock] used to manipulate the [_items].
 class _DockState<T extends Object> extends State<Dock<T>> {
-  /// [T] items being manipulated.
   late final List<T> _items = widget.items.toList();
   late final ValueNotifier<List<T>> _itemsNotifier;
-  final double itemHeight = 64;
+
+  late final double itemHeight = widget.itemHeight;
+
   int? hoveredIndex;
   T? draggedItem;
-  bool get isDragged => draggedItem != null;
+  bool get isAnyDragged => draggedItem != null;
   bool isDragOutside = false;
-
-  void _updateItems(int oldIndex, int newIndex) {
-    final T item = _items.removeAt(oldIndex);
-    _items.insert(newIndex, item);
-  }
-
-  int _calculateHoverDistance(int index) =>
-      hoveredIndex == null ? -69 : (index - hoveredIndex!).abs();
-
-  void _setIsDragOutside(bool value) {
-    if (isDragged) {
-      setState(() {
-        if (!value) {
-          hoveredIndex = null;
-        }
-        isDragOutside = value;
-      });
-    }
-  }
 
   @override
   void initState() {
@@ -76,14 +62,16 @@ class _DockState<T extends Object> extends State<Dock<T>> {
           valueListenable: _itemsNotifier,
           builder: (context, List<T> items, child) {
             return AnimatedReorderableListView(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
               items: items,
+              shrinkWrap: true,
               longPressDraggable: false,
+              buildDefaultDragHandles: false,
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              isSameItem: (T a, T b) => a.toString() == b.toString(),
               onReorder: (int oldIndex, int newIndex) {
-                _updateItems(oldIndex, newIndex);
-              }, //TODO
+                _moveItem(oldIndex, newIndex);
+              },
               onReorderStart: (p0) {
                 draggedItem = items.elementAtOrNull(p0);
                 hoveredIndex = null;
@@ -92,25 +80,21 @@ class _DockState<T extends Object> extends State<Dock<T>> {
                 hoveredIndex = null;
                 draggedItem = null;
               },
-              isSameItem: (T a, T b) => a.toString() == b.toString(),
-              buildDefaultDragHandles: false,
-              itemBuilder: (context, index) {
-                return Draggable<T>(
-                  key: ValueKey(_items[index].toString()),
-                  data: _items[index],
-                  feedback: _getFeedbackItem(_items[index], itemHeight),
-                  child: _getItem(_items[index], index, itemHeight),
-                  onDragStarted: () {
-                    draggedItem = _items[index];
-                  },
-                  onDragEnd: (details) {
-                    setState(() {
-                      draggedItem = null;
-                      hoveredIndex = null;
-                    });
-                  },
-                );
-              },
+              itemBuilder: (context, index) => Draggable<T>(
+                key: ValueKey(_items[index].toString()),
+                data: _items[index],
+                feedback: _getFeedbackItem(_items[index], itemHeight),
+                child: _getItem(_items[index], index, itemHeight),
+                onDragStarted: () {
+                  draggedItem = _items[index];
+                },
+                onDragEnd: (details) {
+                  setState(() {
+                    draggedItem = null;
+                    hoveredIndex = null;
+                  });
+                },
+              ),
             );
           },
         ),
@@ -118,6 +102,35 @@ class _DockState<T extends Object> extends State<Dock<T>> {
     );
   }
 
+  /// Helper to change item order
+  void _moveItem(int oldIndex, int newIndex) {
+    final T item = _items.removeAt(oldIndex);
+    _items.insert(newIndex, item);
+  }
+
+  /// Helper to update flag if the item is being dragged outside the dock
+  void _setIsDragOutside(bool value) {
+    if (isAnyDragged) {
+      setState(() {
+        if (!value) {
+          hoveredIndex = null;
+        }
+        isDragOutside = value;
+      });
+    }
+  }
+
+  /// Helper to calculate distance from hovered item
+  int _calculateHoverDistance(int index) =>
+      hoveredIndex == null ? -69 : (index - hoveredIndex!).abs();
+
+  /// Helper to determine if the item is being dragged
+  bool _isItemDragged(T item) => draggedItem?.toString() == item.toString();
+
+  /// Helper to determine if the item should be compacted
+  bool _isCompacted(bool isItemDragged) => isItemDragged && isDragOutside;
+
+  /// FeedbackItem widget
   Widget _getFeedbackItem(T item, double size) {
     return Transform.scale(
       scale: 1.2,
@@ -129,51 +142,38 @@ class _DockState<T extends Object> extends State<Dock<T>> {
     );
   }
 
+  /// DockItem widget
   Widget _getItem(T item, int index, double height) {
-    bool isItemDragged = draggedItem.toString() == item.toString();
-    bool isCompacted = isItemDragged && isDragOutside;
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      curve: Curves.easeInOut,
+    return DockItem<T>(
+      index: index,
+      item: item,
       height: height,
-      width: isCompacted ? 0 : height,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          DragTarget<T>(
-            onAcceptWithDetails: (details) {
-              draggedItem = null;
-              final int fromIndex = _items.indexOf(details.data);
-              _updateItems(fromIndex, index);
-              _itemsNotifier.value = List.from(_items);
-            },
-            onWillAcceptWithDetails: (details) {
-              setState(() {
-                hoveredIndex = index;
-              });
+      builder: widget.builder,
+      distance: _calculateHoverDistance(index),
+      isCompacted: _isCompacted(_isItemDragged(item)),
+      isDragged: _isItemDragged(item),
+      isAnyDragged: isAnyDragged,
+      onAcceptWithDetails: (T data) {
+        draggedItem = null;
+        final int fromIndex = _items.indexOf(data);
+        _moveItem(fromIndex, index);
+        _itemsNotifier.value = List.from(_items);
+      },
+      onWillAcceptWithDetails: (T data) {
+        setState(() {
+          hoveredIndex = index;
+        });
 
-              final int fromIndex = _items.indexOf(details.data);
-              _updateItems(fromIndex, index);
-              _itemsNotifier.value = List.from(_items);
-              return true;
-            },
-            builder: (context, candidateData, rejectedData) {
-              return Container();
-            },
-          ),
-          HoverableWrapper(
-            index: index,
-            distance: _calculateHoverDistance(index),
-            onHover: (int? i) {
-              setState(() {
-                hoveredIndex = i;
-              });
-            },
-            isAnyDragged: isDragged,
-            child: isItemDragged ? Container() : widget.builder(item, height),
-          ),
-        ],
-      ),
+        final int fromIndex = _items.indexOf(data);
+        _moveItem(fromIndex, index);
+        _itemsNotifier.value = List.from(_items);
+        return true;
+      },
+      onHover: (int? i) {
+        setState(() {
+          hoveredIndex = i;
+        });
+      },
     );
   }
 }
